f032529da40a7adac54392445cc21f40ab1c1360
Alex Brackenridge <a.brack@live.co.uk>
2023-05-31 13:52:16 +0100

    Added student controller modify methods

--- src/main/java/com/sms/studentmanagementsystem/Controller/StudentController.java
+++ src/main/java/com/sms/studentmanagementsystem/Controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.sms.studentmanagementsystem.Controller;
 
 import com.sms.studentmanagementsystem.Entity.Student;
+import com.sms.studentmanagementsystem.Repository.StudentRepository;
 import com.sms.studentmanagementsystem.Service.StudentService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,10 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController {
 
     private StudentService studentService;
+    private final StudentRepository studentRepository;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService,
+                             StudentRepository studentRepository) {
         super();
         this.studentService = studentService;
+        this.studentRepository = studentRepository;
     }
 
     @GetMapping("/students")
@@ -49,6 +53,22 @@ public class StudentController {
     public String updateStudent(@PathVariable Long id, Model model){
         model.addAttribute("student", studentService.getStudentById(id));
         return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String modifyStudent(@PathVariable Long id,
+                                @ModelAttribute("student") Student student,
+                                Model model){
+
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        studentRepository.save(existingStudent);
+
+        return "redirect:/students";
+
     }
 
 }
